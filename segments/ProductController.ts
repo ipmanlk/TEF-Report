dd6fbbe0572e102fe7f0076ac08f54bc41fedba2
ipmanlk <io@navinda.xyz>
2021-01-20 21:07:47 +0530

    fix: code segments size

--- segments/ProductController.ts
+++ segments/ProductController.ts
@@ -1,9 +1,7 @@
 import { getRepository } from "typeorm";
-import { Product } from "../entity/Product";
-import { ProductStatus } from "../entity/ProductStatus";
-import { ProductDao } from "../dao/ProductDao";
-import { ValidationUtil } from "../util/ValidationUtil";
-import { MiscUtil } from "../util/MiscUtil";
+import { Product, ProductStatus } from "../entity";
+import { ProductDao } from "../dao";
+import { ValidationUtil, MiscUtil } from "../util";
 
 export class ProductController {
 	// determine if this is a request for a single object or a search
@@ -33,12 +31,6 @@ export class ProductController {
 
 		// check if entry exists
 		if (product !== undefined) {
-			// remove useless elements
-			product[
-				"createdEmployee"
-			] = `${product.employee.callingName} (${product.employee.number})`;
-			delete product.employee;
-
 			return {
 				status: true,
 				data: product,
@@ -74,18 +66,8 @@ export class ProductController {
 		data.employeeId = session.data.employeeId;
 		// create product object
 		const product = data as Product;
-		// extract photo
-		const { photo } = data;
-		// calculate photo size in kb
-		if (photo.length > 689339) {
-			throw {
-				status: false,
-				type: "input",
-				msg: "Your photo should be smaller than 500KB.",
-			};
-		}
 		// read photo as buffer
-		const decodedBase64 = MiscUtil.decodeBase64Image(photo);
+		const decodedBase64 = MiscUtil.decodeBase64Image(data.photo);
 		product.photo = decodedBase64.data;
 		// generate product code
 		const lastProduct = await getRepository(Product).findOne({
@@ -108,15 +90,6 @@ export class ProductController {
 			};
 		} catch (e) {
 			console.log(e);
-
-			if (e.code == "ER_DUP_ENTRY") {
-				const msg = await this.getDuplicateErrorMsg(e, product);
-				throw {
-					status: false,
-					type: "input",
-					msg: msg,
-				};
-			}
 			throw {
 				status: false,
 				type: "server",
@@ -151,19 +124,10 @@ export class ProductController {
 		if (data.photo == false) {
 			editedProduct.photo = selectedProduct.photo;
 		} else {
-			// calculate photo size in kb
-			if (data.photo.length > 689339) {
-				throw {
-					status: false,
-					type: "input",
-					msg: "Your photo should be smaller than 500KB.",
-				};
-			}
 			// read photo as buffer
 			const decodedBase64 = MiscUtil.decodeBase64Image(data.photo);
 			editedProduct.photo = decodedBase64.data;
 		}
-
 		// check if valid data is given
 		await ValidationUtil.validate("PRODUCT", editedProduct);
 
@@ -174,14 +138,6 @@ export class ProductController {
 				msg: "That product has been updated!",
 			};
 		} catch (e) {
-			if (e.code == "ER_DUP_ENTRY") {
-				const msg = await this.getDuplicateErrorMsg(e, editedProduct);
-				throw {
-					status: false,
-					type: "input",
-					msg: msg,
-				};
-			}
 			throw {
 				status: false,
 				type: "server",
@@ -220,15 +176,6 @@ export class ProductController {
 					msg: "Server Error!. Please check logs.",
 				};
 			});
-		// if there is no status called deleted
-		if (!deletedStatus) {
-			throw {
-				status: false,
-				type: "server",
-				msg: "Deleted status doesn't exist in the database!.",
-			};
-		}
-
 		// delete the product
 		product.productStatus = deletedStatus;
 		await getRepository(Product)
@@ -245,29 +192,5 @@ export class ProductController {
 			status: true,
 			msg: "That product has been deleted!",
 		};
-	}
-
-	private static async getDuplicateErrorMsg(error, product: Product) {
-		// check if which unique field is violated
-		let msg, field, duplicateEntry;
-		if (error.sqlMessage.includes("code_UNIQUE")) {
-			field = "code";
-			msg = "Product code already exists!";
-		}
-		// get duplicated entry
-		duplicateEntry = await getRepository(Product)
-			.findOne({
-				select: ["id", "code"],
-				where: { field: product[field] },
-			})
-			.catch((e) => {
-				console.log(e);
-				throw {
-					status: false,
-					type: "server",
-					msg: "Server Error!. Please check logs.",
-				};
-			});
-		return `Product (code: ${duplicateEntry.code}) with the same ${msg}`;
 	}
 }
--- segments/ProductDao.ts
+++ segments/ProductDao.ts
@@ -1,6 +1,5 @@
 import { getRepository } from "typeorm";
-import { Product } from "../entity/Product";
-
+import { Product } from "../entity";
 export class ProductDao {
 	// search products and return an array of objects
 	static search({ keyword = "", skip = 0, limit = 15 }) {
@@ -8,14 +7,9 @@ export class ProductDao {
 			return getRepository(Product)
 				.createQueryBuilder("p")
 				.select(["p.id", "p.code", "p.name", "p.cost", "p.price"])
-				.leftJoinAndSelect("p.productStatus", "ps")
-				.leftJoinAndSelect("p.category", "ca")
 				.where("p.code LIKE :keyword", { keyword: `%${keyword}%` })
 				.orWhere("p.name LIKE :keyword", { keyword: `%${keyword}%` })
-				.orWhere("p.cost LIKE :keyword", { keyword: `%${keyword}%` })
 				.orWhere("p.price LIKE :keyword", { keyword: `%${keyword}%` })
-				.orWhere("ps.name LIKE :keyword", { keyword: `%${keyword}%` })
-				.orWhere("ca.name LIKE :keyword", { keyword: `%${keyword}%` })
 				.skip(skip)
 				.take(limit)
 				.getMany();
